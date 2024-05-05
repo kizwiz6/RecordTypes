21ec1a43336f683489dd3cbc2ea45be6e5d4e1b1
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-06 00:35:52 +0100

    Added Last Name parameter details, alongside some error handling in the constructor.

--- RecordTypes/Member.cs
+++ RecordTypes/Member.cs
@@ -9,7 +9,8 @@ namespace RecordTypes
     public class Member
     {
         public int Id { get; init; } // init-only setter
-        public string Name { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
         public string Address { get; set; }
 
         /// <summary>
@@ -18,13 +19,37 @@ namespace RecordTypes
         /// <param name="memberId">makes Id property Immutable</param>
         public Member(int memberId)
         {
-            Id = memberId;
+            if (memberId <= 0)
+            {
+                Id = memberId;
+            }
         }
 
-        public Member(int memberId, string name, string address)
+        public Member(int memberId, string firstName, string lastName, string address)
         {
+            if (memberId <= 0)
+            {
+                throw new ArgumentException("Member ID must be a positive integer.", nameof(memberId));
+            }
+
+            if(string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException("First name cannot be null or empty.", nameof(firstName));
+            }
+
+            if(string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentException("Last name cannot be null or empty.", nameof(firstName));
+            }
+
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                throw new ArgumentException("Address cannot be null or empty.", nameof(address));
+            }
+
             this.Id = memberId;
-            this.Name = name;
+            this.FirstName = firstName;
+            this.LastName = lastName;
             this.Address = address;
         }
     }
--- RecordTypes/Program.cs
+++ RecordTypes/Program.cs
@@ -4,16 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Member memberObj = new Member(1, "Kieran", "Cambridgeshire");
+            while (true)
+            {
+                Member memberObj = new Member(0, "", "Emery", "Cambridgeshire");
 
-            Console.WriteLine("****************START - Member Details***********");
+                Console.WriteLine("****************START - Member Details***********");
 
-            Console.WriteLine("ID:- " + memberObj.Id);
-            Console.WriteLine("Name:- " + memberObj.Name);
-            Console.WriteLine("Address:- " + memberObj.Address);
-            Console.ReadLine();
+                Console.WriteLine("ID:- " + memberObj.Id);
+                Console.WriteLine("First Name:- " + memberObj.FirstName);
+                Console.WriteLine("Last Name:- " + memberObj.LastName);
+                Console.WriteLine("Address:- " + memberObj.Address);
+                Console.ReadLine();
 
-            Console.WriteLine("****************END - Member Details***********");
+                Console.WriteLine("****************END - Member Details***********");
+
+            }
+
+
+
+            //var newMember = new Member
+            //{
+            //    Id = member.Id,
+            //    FirstName = member.FirstName,
+            //    LastName = member.LastName,
+            //    Address = "Mumbai"
+            //};
         }
     }
 }
