5db46591f0e691efb0fafceaff319c6f5d4fc5b9
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-06 00:23:25 +0100

    Add project files.

---
+++ RecordTypes/Member.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordTypes
+{
+    public class Member
+    {
+        public int Id { get; init; } // init-only setter
+        public string Name { get; init; }
+        public string Address { get; set; }
+
+        /// <summary>
+        /// If we want to make properties immutable in the traditional way, we have to pass values in constructor like the below code.
+        /// </summary>
+        /// <param name="memberId">makes Id property Immutable</param>
+        public Member(int memberId)
+        {
+            Id = memberId;
+        }
+
+        public Member(int memberId, string name, string address)
+        {
+            this.Id = memberId;
+            this.Name = name;
+            this.Address = address;
+        }
+    }
+}
---
+++ RecordTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace RecordTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Member memberObj = new Member(1, "Kieran", "Cambridgeshire");
+
+            Console.WriteLine("****************START - Member Details***********");
+
+            Console.WriteLine("ID:- " + memberObj.Id);
+            Console.WriteLine("Name:- " + memberObj.Name);
+            Console.WriteLine("Address:- " + memberObj.Address);
+            Console.ReadLine();
+
+            Console.WriteLine("****************END - Member Details***********");
+        }
+    }
+}
